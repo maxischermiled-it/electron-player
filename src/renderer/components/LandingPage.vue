4e59c063d86f1d12f3e7d3999a940c5426ac3080
antonio caccamo <caccamo.antonio@gmail.com>
2021-05-14 17:58:40 +0200

    WIP : Dimension Card

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -8,66 +8,23 @@
       <b-tab-item 
         v-for="viewer in viewers"
         :key="viewer.index"
-        v-bind:label=" 'Screen #' + (viewer.index + 1)" 
+        v-bind:label=" 'Screen #' + (viewer.index )" 
       >
-        <div class="card">
-          <header class="card-header"><p class="card-header-title">Dimensions</p></header>
-          <div class="card-content columns">
-            <div class="column is-half">
-              <div class="card">
-                <header class="card-header"><p class="card-header-title">Size</p></header>
-                <div class="card-content">
-                  <div class="content">
-                    <div class="columns">
-                      <div class="column is-half">
-                        <b-field label="width">
-                          <b-numberinput controls-position="compact" controls-rounded :value="viewer.bounds.width" @input="onChange(viewer.index, 'bounds.width', $event)"></b-numberinput>
-                        </b-field>
-                      </div>
-                      <div class="column is-half">
-                        <b-field label="height">
-                          <b-numberinput controls-position="compact" controls-rounded :value="viewer.bounds.height" @input="onChange(viewer.index, 'bounds.height', $event)"></b-numberinput>
-                        </b-field>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="column is-half">
-              <div class="card">
-                <header class="card-header"><p class="card-header-title">Poistion</p></header>
-                <div class="card-content">
-                  <div class="content">
-                    <div class="columns">
-                      <div class="column is-half">
-                        <b-field label="top">
-                          <b-numberinput controls-position="compact" controls-rounded :value="viewer.bounds.top" @input="onChange(viewer.index, 'bounds.top', $event)"></b-numberinput>
-                        </b-field>
-                      </div>
-                      <div class="column is-half">
-                        <b-field label="left">
-                           <b-numberinput controls-position="compact" controls-rounded :value="viewer.bounds.left" @input="onChange(viewer.index, 'bounds.left', $event)"></b-numberinput>
-                        </b-field>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <dimension-card :viewer="viewer"></dimension-card>
+        <dimension-card :viewer="viewer"></dimension-card>
       </b-tab-item>
     </b-tabs>
   </div>
 </template>
 
 <script>
+import DimensionCard from './LandingPage/DimensionCard'
 
 export default {
+  components: {DimensionCard},
   data () {
     return {
-      subscription: null,
+      subscrDimensionCardiption: null,
       expandOnHover: false,
       mobile: 'reduce',
       reduce: true
---
+++ src/renderer/components/LandingPage/DimensionCard.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">Dimensions</p>
+    </header>
+    <div class="card-content columns">
+      <div class="column is-half">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">Size</p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="columns">
+                <div class="column is-half">
+                  <b-field label="width">
+                    <b-numberinput
+                      controls-position="compact"
+                      controls-rounded
+                      :value="viewer.bounds.width"
+                      @input="onChange('bounds.width', $event)"
+                    ></b-numberinput>
+                  </b-field>
+                </div>
+                <div class="column is-half">
+                  <b-field label="height">
+                    <b-numberinput
+                      controls-position="compact"
+                      controls-rounded
+                      :value="viewer.bounds.height"
+                      @input="onChange('bounds.height', $event)"
+                    ></b-numberinput>
+                  </b-field>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-half">
+        <div class="card">
+          <header class="card-header">
+            <p class="card-header-title">Poistion</p>
+          </header>
+          <div class="card-content">
+            <div class="content">
+              <div class="columns">
+                <div class="column is-half">
+                  <b-field label="top">
+                    <b-numberinput
+                      controls-position="compact"
+                      controls-rounded
+                      :value="viewer.bounds.top"
+                      @input="onChange('bounds.top', $event)"
+                    ></b-numberinput>
+                  </b-field>
+                </div>
+                <div class="column is-half">
+                  <b-field label="left">
+                    <b-numberinput
+                      controls-position="compact"
+                      controls-rounded
+                      :value="viewer.bounds.left"
+                      @input="onChange('bounds.left', $event)"
+                    ></b-numberinput>
+                  </b-field>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    viewer: Object
+  },
+  data () {
+    return {
+      index: this.viewer.index
+    }
+  },
+  methods: {
+    onChange (field, value) {
+      var params = {
+        index: this.index,
+        field: field,
+        value: value
+      }
+      console.log('changed bounds :' + JSON.stringify(params))
+      this.$store.dispatch('player/updateViewerField', params)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
